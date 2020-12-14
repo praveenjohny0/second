378d263f4145d367c167eacfb16e7e2d7f0d3b76
praveenjohny0 <praveenjohny0@gmail.com>
2020-12-14 11:33:07 +0530

    Router Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import './New.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import List from './Component/List';
+import Form from './Component/Form';
+import Home from './Component/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MAIN PAGE</h1>
+      <div className="NavBar">
+        <Router>
+          <div>
+            <nav>
+              <ul>
+
+                <Link to="/" className="link">Home</Link>
+
+                <Link to="/Form" className="link">Form</Link>
+
+                <Link to="/List" className="link">User List</Link>
+
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route path="/List">
+                <List />
+              </Route>
+              <Route path="/Form">
+                <Form />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
